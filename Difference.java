ed4964c029590df7a9056d0a3ffb91f5f5a9d674
Shivam P Baghele <baghelesp@rknec.edu>
2022-08-18 07:36:02 +0530

    added implementation subtraction

--- Difference.java
+++ Difference.java
@@ -1,5 +1,12 @@
+import java.util.*;
 public class Difference {
     public static void main(String[] args) {
-        System.err.println(5-6);
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter first integer : ");
+        int a= sc.nextInt();
+        System.out.print("Enter Second integer : ");
+        int b=sc.nextInt();
+        int c= a-b;
+        System.out.println(a+" - "+b+" = "+c);
     }
 }
